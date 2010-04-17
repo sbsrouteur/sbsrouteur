91c75f7c351250fd45dcea32423c93a79dee5f53
sbs <sbs@34ec630f-655c-4738-8800-74af9c3ddee0>
2010-04-17 08:33:36 +0000

    Posted V0.16

--- Routeur/Classes/Configuration/TextToNumberConverter.vb
+++ Routeur/Classes/Configuration/TextToNumberConverter.vb
@@ -2,7 +2,7 @@
 
     Implements IValueConverter
 
-
+    'post V0.16
     Public Function Convert(ByVal value As Object, ByVal targetType As System.Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements System.Windows.Data.IValueConverter.Convert
         Dim i As Integer = 0
 
@@ -16,4 +16,5 @@
     Public Function ConvertBack(ByVal value As Object, ByVal targetType As System.Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements System.Windows.Data.IValueConverter.ConvertBack
         Return value
     End Function
+
 End Class
