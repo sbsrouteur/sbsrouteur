036c4cf9b4742910afee7340225c140791bc06de
sbs <sbs@34ec630f-655c-4738-8800-74af9c3ddee0>
2010-08-24 21:01:33 +0000

    Commit before trashing first try at 7 seg display

---
+++ Routeur/Controls/7SegDisplays/WindDisplay.xaml.vb
@@ -0,0 +1,21 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+
+Partial Public Class WindDisplay
+	Public Sub New()
+		MyBase.New()
+
+		Me.InitializeComponent()
+
+		' Insérez le code requis pour la création d’objet sous ce point.
+	End Sub
+End Class
