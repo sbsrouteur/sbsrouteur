e0932f668029ac3115f18925dc3770707e68dc4b
sbs <sbs@34ec630f-655c-4738-8800-74af9c3ddee0>
2010-11-07 08:15:33 +0000

    Fix iso end condition

---
+++ Routeur/Classes/GSHHS/TileServer/TileInfo.vb
@@ -0,0 +1,46 @@
+﻿Imports System.IO
+
+Public Class TileInfo
+
+    Private _BaseTilesPath As String = RouteurModel.BaseFileDir & "\tiles"
+
+    Private _Z As Integer
+    Private _Center As Coords
+
+    Public Sub New(ByVal z As Integer, ByVal TileCenter As Coords)
+
+        _Z = z
+        _Center = TileCenter
+
+    End Sub
+
+    Public Function FileName() As String
+        Return Path.Combine(BaseTilesPath, TilePath)
+    End Function
+
+    Public ReadOnly Property BaseTilesPath() As String
+        Get
+            Return _BaseTilesPath
+        End Get
+    End Property
+    Public ReadOnly Property Center() As Coords
+        Get
+            Return _Center
+        End Get
+    End Property
+
+    Public ReadOnly Property TilePath() As String
+
+        Get
+            Return Z.ToString("000") & "_" & Center.Lat_Deg.ToString &"_"&Center.Lon_Deg 
+        End Get
+
+    End Property
+
+    Public ReadOnly Property Z() As Integer
+        Get
+            Return _Z
+        End Get
+    End Property
+
+End Class
---
+++ Routeur/Classes/GSHHS/TileServer/TileServer.vb
@@ -0,0 +1,77 @@
+﻿Imports System.IO
+Imports System.Drawing
+
+Public Class TileServer
+
+    Public Event TileReady(ByVal tilename As String, ByVal Tile As Image)
+
+    Public Const TILE_SIZE As Integer = 256
+
+    Private _Renderer As _2D_Viewer
+
+    Private _TileBuildList As New SortedList(Of String, String)
+
+    Private Sub BgCreateTile(ByVal state As Object)
+
+        Dim TI As TileInfo = CType(state, TileInfo)
+        SyncLock _TileBuildList
+
+            If _TileBuildList.ContainsKey(TI.TilePath) Then
+                Return
+            Else
+                _TileBuildList.Add(TI.TilePath, TI.TilePath)
+            End If
+        End SyncLock
+
+        Dim CenterPoint As New PointF
+        CenterPoint.X = CSng(_Renderer.LonToCanvas(TI.Center.Lon_Deg))
+        CenterPoint.Y = CSng(_Renderer.LatToCanvas(TI.Center.Lat_Deg))
+
+        Dim North As Double = _Renderer.CanvasToLon(CenterPoint.X + TILE_SIZE / 2)
+        Dim South As Double = _Renderer.CanvasToLon(CenterPoint.X - TILE_SIZE / 2)
+        Dim East As Double = _Renderer.CanvasToLat(CenterPoint.Y - TILE_SIZE / 2)
+        Dim West As Double = _Renderer.CanvasToLat(CenterPoint.Y - TILE_SIZE / 2)
+
+        Dim W As Double = East - West
+        Dim H As Double = North - South
+        Dim Resolution As Double = If(W < H, W, H) / TILE_SIZE
+        Dim XOffset As Double = CenterPoint.X - TILE_SIZE / 2
+        Dim YOffset As Double = CenterPoint.Y - TILE_SIZE / 2
+        Dim img As New Bitmap(TILE_SIZE, TILE_SIZE, Imaging.PixelFormat.Format32bppArgb)
+
+        Dim MapLevel As String
+
+        Select Case TI.Z
+            Case Else
+                MapLevel = "c"
+        End Select
+        MapLevel = "..\gshhs\gshhs_" & MapLevel & ".b"
+        GSHHS_Reader.ReadTile(YOffset, XOffset, _Renderer, North, South, East, West, img, MapLevel)
+
+        If Not Directory.Exists(TI.BaseTilesPath) Then
+            Directory.CreateDirectory(TI.BaseTilesPath)
+        End If
+        img.Save(TI.FileName)
+
+    End Sub
+
+    Public Sub New(ByVal Render As _2D_Viewer)
+
+        _renderer = Render
+
+    End Sub
+
+    Public Sub RequestTile(ByVal TI As TileInfo)
+
+
+        If File.Exists(TI.FileName) Then
+            Dim Tile As New Bitmap(TI.FileName)
+
+            RaiseEvent TileReady(TI.TilePath, Tile)
+        Else
+            System.Threading.ThreadPool.QueueUserWorkItem(AddressOf BgCreateTile, TI)
+        End If
+
+    End Sub
+
+End Class
