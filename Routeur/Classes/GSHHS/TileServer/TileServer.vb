50fe298c29a8fa094432baae2819d5aeb4f04dda
sbs <sbs@34ec630f-655c-4738-8800-74af9c3ddee0>
2010-11-09 22:22:03 +0000

    Tiles daily commit

--- Routeur/Classes/GSHHS/TileServer/TileServer.vb
+++ Routeur/Classes/GSHHS/TileServer/TileServer.vb
@@ -3,13 +3,15 @@ Imports System.Drawing
 
 Public Class TileServer
 
-    Public Event TileReady(ByVal tilename As String, ByVal Tile As Image)
+    Public Event TileReady(ByVal ti As TileInfo)
 
     Public Const TILE_SIZE As Integer = 256
 
     Private _Renderer As _2D_Viewer
 
     Private _TileBuildList As New SortedList(Of String, String)
+    Private _queryCount As Long = 0
+    Private _HitCount As Long = 0
 
     Private Sub BgCreateTile(ByVal state As Object)
 
@@ -23,11 +25,10 @@ Public Class TileServer
             End If
         End SyncLock
 
-        Dim MapWidth As Double = 360 / 2 ^ (TI.Z + 1)
-        Dim North As Double = TI.Center.Lat_Deg + MapWidth / 2
-        Dim South As Double = TI.Center.Lat_Deg - MapWidth / 2
-        Dim East As Double = TI.Center.Lon_Deg + MapWidth
-        Dim West As Double = TI.Center.Lon_Deg - MapWidth
+        Dim North As Double = TI.North
+        Dim South As Double = TI.South
+        Dim East As Double = TI.East
+        Dim West As Double = TI.West
 
         Dim W As Double = East - West
         Dim H As Double = North - South
@@ -39,10 +40,24 @@ Public Class TileServer
         Dim MapLevel As String
 
         Select Case TI.Z
-            Case Else
+            Case 0 To 3
                 MapLevel = "c"
+            Case 4 To 11
+                MapLevel = "l"
+
+            Case 12 To 14
+                MapLevel = "i"
+            Case 15 To 21
+
+                MapLevel = "h"
+            Case Else
+                MapLevel = "f"
         End Select
         MapLevel = "..\gshhs\gshhs_" & MapLevel & ".b"
+        If Not File.Exists(MapLevel) Then
+            MapLevel = "i"
+            MapLevel = "..\gshhs\gshhs_" & MapLevel & ".b"
+        End If
         GSHHS_Reader.ReadTile(YOffset, XOffset, _Renderer, North, South, East, West, img, MapLevel)
 
         If Not Directory.Exists(TI.BaseTilesPath) Then
@@ -53,6 +68,7 @@ Public Class TileServer
         SyncLock _TileBuildList
             _TileBuildList.Remove(TI.TilePath)
         End SyncLock
+        RaiseEvent TileReady(TI)
     End Sub
 
     Public Sub New(ByVal Render As _2D_Viewer)
@@ -63,16 +79,35 @@ Public Class TileServer
 
     Public Sub RequestTile(ByVal TI As TileInfo)
 
-
+        _queryCount += 1
         If File.Exists(TI.FileName) Then
             Dim Tile As New Bitmap(TI.FileName)
-
-            RaiseEvent TileReady(TI.TilePath, Tile)
+            _HitCount += 1
+            RaiseEvent TileReady(TI)
         Else
-            System.Threading.ThreadPool.QueueUserWorkItem(AddressOf BgCreateTile, TI)
-        End If
 
+#Const MONO_THREAD_TILES = 0
+
+#If MONO_THREAD_TILES Then
+            BgCreateTile (TI)
+#Else
+            System.Threading.ThreadPool.QueueUserWorkItem(AddressOf BgCreateTile, TI)
+
+#End If
+
+        End If
+        Debug.WriteLine("tileserver Q;" & _queryCount & " " & _HitCount / _queryCount)
     End Sub
+
+    Public ReadOnly Property TileServerStateIdle() As Boolean
+        Get
+            SyncLock _TileBuildList
+                Return _TileBuildList.Count = 0
+            End SyncLock
+
+        End Get
+    End Property
+
 
     
 End Class
