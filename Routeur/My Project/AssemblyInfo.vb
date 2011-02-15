ee4bd33d808ece0b881a6ec79690466b378bc370
sbs <sbs@34ec630f-655c-4738-8800-74af9c3ddee0>
2011-02-15 07:28:30 +0000

    V25 Posted

--- Routeur/My Project/AssemblyInfo.vb
+++ Routeur/My Project/AssemblyInfo.vb
@@ -56,5 +56,5 @@ Imports System.Windows
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.24.4.0")> 
-<Assembly: AssemblyFileVersion("0.24.4.0")> 
+<Assembly: AssemblyVersion("0.25.0.0")> 
+<Assembly: AssemblyFileVersion("0.25.0.0")> 
