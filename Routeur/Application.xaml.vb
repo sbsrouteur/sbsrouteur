e8740eda81b8fe9a5f7a2fa77fd6de91ca478c37
sbs <sbs@34ec630f-655c-4738-8800-74af9c3ddee0>
2012-02-29 06:17:43 +0000

    Change to Arch specific build, update setup adding MVCR10 reference, add generic generic handler

--- Routeur/Application.xaml.vb
+++ Routeur/Application.xaml.vb
@@ -12,5 +12,18 @@ Class Application
                   New FrameworkPropertyMetadata( _
                      XmlLanguage.GetLanguage( _
                      CultureInfo.CurrentCulture.IetfLanguageTag)))
+
+        Dim currentDomain As AppDomain = AppDomain.CurrentDomain
+        AddHandler currentDomain.UnhandledException, AddressOf CrashCatcher
     End Sub
+
+    Private Sub Application_DispatcherUnhandledException(sender As Object, e As System.Windows.Threading.DispatcherUnhandledExceptionEventArgs) Handles Me.DispatcherUnhandledException
+        MessageBox.Show("App unhandled exception :" & e.Exception.Message & vbCrLf & e.Exception.StackTrace)
+    End Sub
+
+    Private Sub CrashCatcher(sender As Object, ev As UnhandledExceptionEventArgs)
+        Dim e As Exception = CType(ev.ExceptionObject, Exception)
+        MessageBox.Show("App unhandled exception :" & e.Message & vbCrLf & e.StackTrace)
+    End Sub
+
 End Class
