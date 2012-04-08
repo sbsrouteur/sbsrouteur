2a27a71084a7ac8b70b4692aafeb22123e7b8926
sbs <sbs@34ec630f-655c-4738-8800-74af9c3ddee0>
2012-04-08 19:55:43 +0000

    Push comment from Hg

--- Routeur/Classes/BoatInfo/BoatInfo.vb
+++ Routeur/Classes/BoatInfo/BoatInfo.vb
@@ -4,6 +4,8 @@ Imports System.Net
 
 Public Class BoatInfo
 
+    'HgPush
+
     Implements INotifyPropertyChanged
 
     Public Event PropertyChanged(ByVal sender As Object, ByVal e As System.ComponentModel.PropertyChangedEventArgs) Implements System.ComponentModel.INotifyPropertyChanged.PropertyChanged
