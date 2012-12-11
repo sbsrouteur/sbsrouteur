050cd6e4c85a55d0d01caa079b909ae5d5ca45b0
sbs <sbs@34ec630f-655c-4738-8800-74af9c3ddee0>
2012-12-11 16:03:05 +0000

    Changed Meteo layer to arrows. Fixed display/positions bugs

--- Routeur/Classes/Meteo/MeteoBitmapper.vb
+++ Routeur/Classes/Meteo/MeteoBitmapper.vb
@@ -15,6 +15,12 @@ Public Class MeteoBitmapper
     Private _StopRender As Boolean = False
     Private _RenderThread As Thread
     Private _ImgData() As Integer
+    Private _renderlock As New Object
+    Private Const NbX As Integer = 20
+    Private Const NbY As Integer = 20
+    Dim WindDir(NbX * NbY - 1) As Double
+    Dim WindSpeed(NbX * NbY - 1) As Double
+
 
     Private _Arrow() As Integer = New Integer() {0, 4,
                                                  0, 3,
@@ -40,15 +46,54 @@ Public Class MeteoBitmapper
                 Dim H As Integer = CInt(Math.Ceiling(_Viewer.ActualHeight))
                 Dim ImgStride As Integer = CInt(W * ((PixelFormats.Pbgra32.BitsPerPixel) / 8))
                 Dim R As New Int32Rect(0, 0, CInt(W), CInt(H))
+                Dim CPt(3) As Point
+                CPt(0) = New Point(0, -20)
+                CPt(1) = New Point(-5, 5)
+                CPt(2) = New Point(0, 0)
+                CPt(3) = New Point(5, 5)
+                Dim d(3) As Double
 
-
+                For n As Integer = 0 To 3
+                    d(n) = Sqrt(CPt(n).X * CPt(n).X + CPt(n).Y * CPt(n).Y)
+                Next
                 Debug.Assert(Thread.CurrentThread.ManagedThreadId = _Viewer.Dispatcher.Thread.ManagedThreadId)
 
-                If _Img Is Nothing Then
-                    _Img = New WriteableBitmap(CInt(W), CInt(H), 96, 96, PixelFormats.Pbgra32, Nothing)
+                If _Img Is Nothing OrElse _Img.Width <> W OrElse _Img.Height <> H Then
+                    _Img = New WriteableBitmap(W, H, 96, 96, PixelFormats.Pbgra32, Nothing)
                 End If
                 Using _Img.GetBitmapContext
-                    _Img.WritePixels(R, _ImgData, ImgStride, 0)
+                    '_Img.WritePixels(R, _ImgData, ImgStride, 0)
+                    _Img.Clear()
+                    For x As Integer = 0 To NbX - 1
+                        For y As Integer = 0 To NbY - 1
+                            Dim pt(3) As Point
+                            Dim Scale As Double = Math.Log(WindSpeed(x + NbX * y) + 1)
+                            Dim WindColor As Integer = WindColors.GetColor(WindSpeed(x + NbX * y))
+
+                            For n As Integer = 0 To 3
+                                'Scale Arrow
+                                pt(n) = New Point(CPt(n).X * Scale, CPt(n).Y * Scale)
+
+                                'Rotate
+                                Dim a As Double = Atan2(pt(n).Y, pt(n).X) - WindDir(x + NbX * y)
+                                pt(n).X = d(n) * Cos(a)
+                                pt(n).Y = d(n) * Sin(a)
+
+                                'Offset
+                                pt(n).X += W / (NbX - 1) * x
+                                pt(n).Y += H / (NbY - 1) * y
+                            Next
+
+                            _Img.FillQuad(CInt(pt(0).X), CInt(pt(0).Y), CInt(pt(1).X), CInt(pt(1).Y), CInt(pt(2).X), CInt(pt(2).Y), CInt(pt(3).X), CInt(pt(3).Y), WindColor)
+
+                            'Dim Txt As New TextBlock
+                            'Txt.Text = WindSpeed(x + NbX * y).ToString("0Kts") & "/" & (WindDir(x + NbX * y) / PI * 180).ToString("0.0°")
+                            'Txt.Foreground = New SolidColorBrush(Color.FromRgb(0, 0, 0))
+                            'Txt.FontSize = 7
+                            'Dim tbmp As New WriteableBitmap(40, 10, 96, 96, PixelFormats.Pbgra32, Nothing)
+                            'tbmp.render()
+                        Next
+                    Next
                 End Using
 
             Catch ex As Exception
@@ -67,87 +112,75 @@ Public Class MeteoBitmapper
         Dim R As New Int32Rect(0, 0, CInt(_Viewer.ActualWidth), CInt(_Viewer.ActualHeight))
         Dim Dx As Integer = CInt(_Viewer.ActualWidth / 10)
         Dim Dy As Integer = CInt(_Viewer.ActualHeight / 10)
-        Dim Dir(99) As Double
-        For x As Integer = 0 To CInt(_Viewer.ActualWidth - 1) Step Dx
+        Dim Start As DateTime = Now
+        Dim MeteoCount As Integer = 0
+        Dim MeteoMs As Double = 0
+        Dim Ix As Integer = 0
+        Dim Iy As Integer = 0
+        For Ix = 0 To NbX - 1
+            Dim X As Integer = CInt(Ix * _Viewer.ActualWidth / NbX)
 
-            For y As Integer = 0 To CInt(_Viewer.ActualHeight - 1) Step Dy
-                Dim V(3) As Double
+            For Iy = 0 To NbY - 1
+                Dim Y As Integer = CInt(Iy * _Viewer.ActualHeight / NbY)
                 Dim MiFail As Boolean = False
-                For i As Integer = 0 To 1
-                    For j As Integer = 0 To 1
-                        If _StopRender Then Return
-                        Dim mi As MeteoInfo = _meteo.GetMeteoToDate(New Coords(_Viewer.CanvasToLat(y + j * Dy), _Viewer.CanvasToLon(x + i * Dx)), Me.Date, True)
-                        If mi IsNot Nothing Then
-                            V(i + 2 * j) = mi.Strength
-                            Dim Index As Integer = CInt(10 * (y / Dy + j) + (x / Dx + i))
-                            If Index < Dir.Count Then
-                                Dir(Index) = (180 - mi.Dir) / 180 * PI
-                            End If
-                        Else
-                            MiFail = True
-                            Exit For
-                        End If
-                    Next
-                Next
-                If MiFail Then
+
+                If _StopRender Then Return
+                Dim StartMeteo As DateTime = Now
+                Dim mi As MeteoInfo = _meteo.GetMeteoToDate(New Coords(_Viewer.CanvasToLat(Y), _Viewer.CanvasToLon(X)), Me.Date, True)
+                If mi IsNot Nothing Then
+                    WindSpeed(CInt(Ix + NbX * Iy)) = mi.Strength
+                    Dim Index As Integer = NbX * Iy + Ix
+                    If Index < WindDir.Count Then
+                        WindDir(Index) = (180 - mi.Dir) / 180 * PI
+                    End If
+                Else
+                    MiFail = True
                     Exit For
                 End If
-
-                For i = 0 To Dx - 1
-                    Dim S1 As Double = i * (V(1) - V(0)) / (Dx - 1) + V(0)
-                    Dim S2 As Double = i * (V(3) - V(2)) / (Dx - 1) + V(2)
-
-                    For j = 0 To Dy - 1
-
-                        If CInt((y + j) * _Viewer.ActualWidth + x + i) < _ImgData.Length Then
-                            Dim S As Double = (S2 - S1) / (Dy - 1) * j + S1
-                            If S > 70 Then
-                                S = 70
-                            End If
-                            'Dim SIndex As Integer = CInt(Math.Round(S * 10))
-                            'Points(BITMAP_SIZE * j + i) = V
-                            'SyncLock WindColors.WindColorGDIBrushes(SIndex)
-                            ' G.FillRectangle(WindColors.WindColorGDIBrushes(SIndex), New System.Drawing.RectangleF(CSng(i), CSng(j), 1.0, 1.0))
-                            'End SyncLock
-                            Dim C As Integer = WindColors.GetColor(S)
-                            Try
-                                _ImgData(CInt((y + j) * _Viewer.ActualWidth + x + i)) = C
-                            Catch
-                                'something when wrong, return
-                                Return
-                            End Try
-
-                        End If
-
-                    Next
-                Next
-
-            Next
-        Next
-
-        For i = 0 To 99
-            Dim Row As Integer = Dy * CInt(Math.Floor(i / 10))
-            Dim Col As Integer = Dx * (i Mod 10)
-            For p = 0 To _Arrow.Count - 1 Step 2
-                Dim d As Double = Sqrt(_Arrow(p) * _Arrow(p) + _Arrow(p + 1) * _Arrow(p + 1))
-#Const DBG_ARROWS = False
-#If DBG_ARROWS Then
-                Dim a As Double = Atan2(_Arrow(p + 1), _Arrow(p)) + 3.6 * i / 180 * Math.PI
-                
-#Else
-                Dim a As Double = Atan2(_Arrow(p + 1), _Arrow(p)) + Dir(i)
-                
-#End If
-                Dim Px As Integer = CInt(d * Cos(a))
-                Dim Py As Integer = CInt(d * Sin(a))
-                Dim index As Integer = CInt((Row - Py) * _Viewer.ActualWidth + Col + Px)
-                If index > 0 And index < _ImgData.Count Then
-                    _ImgData(index) = &HFF000000
+                If _StopRender Then
+                    Return
                 End If
+                MeteoCount += 1
+                MeteoMs += Now.Subtract(StartMeteo).TotalMilliseconds
+
             Next
 
         Next
+        Console.WriteLine("Meteo avg" & MeteoMs / MeteoCount)
 
+        'For x = 0 To Dx - 1
+        '    Dim S1 As Double = i * (V(1) - V(0)) / (Dx - 1) + V(0)
+        '    Dim S2 As Double = i * (V(3) - V(2)) / (Dx - 1) + V(2)
+
+        '    For j = 0 To Dy - 1
+
+        '        If CInt((y + j) * _Viewer.ActualWidth + x + i) < _ImgData.Length Then
+        '            Dim S As Double = (S2 - S1) / (Dy - 1) * j + S1
+        '            If S > 70 Then
+        '                S = 70
+        '            End If
+        '            'Dim SIndex As Integer = CInt(Math.Round(S * 10))
+        '            'Points(BITMAP_SIZE * j + i) = V
+        '            'SyncLock WindColors.WindColorGDIBrushes(SIndex)
+        '            ' G.FillRectangle(WindColors.WindColorGDIBrushes(SIndex), New System.Drawing.RectangleF(CSng(i), CSng(j), 1.0, 1.0))
+        '            'End SyncLock
+        '            Dim C As Integer = WindColors.GetColor(S)
+        '            Try
+        '                _ImgData(CInt((y + j) * _Viewer.ActualWidth + x + i)) = C
+        '            Catch
+        '                'something when wrong, return
+        '                Return
+        '            End Try
+
+        '        End If
+        '        If _StopRender Then
+        '            Return
+        '        End If
+        '    Next
+        'Next
+
+
+        Console.WriteLine("Meteo duration" & Now.Subtract(Start).TotalSeconds)
     End Sub
 
     Public Property [Date] As DateTime
@@ -178,23 +211,27 @@ Public Class MeteoBitmapper
     Public Sub StartImgRender()
 
         'Debug.Assert(Thread.CurrentThread.ManagedThreadId = _Viewer.Dispatcher.Thread.ManagedThreadId)
-        If _RenderThread IsNot Nothing Then
-            _StopRender = True
-            Try
-                If Not _RenderThread.Join(100) Then
-                    _RenderThread.Abort()
-                End If
-            Catch
-                'ignore this one thread may be gone by itself actually
-            End Try
+        SyncLock _renderlock
+            If _RenderThread IsNot Nothing Then
+                _StopRender = True
+                Try
+                    If Not _RenderThread.Join(100) Then
+                        _RenderThread.Abort()
+                    End If
+                Catch
+                    'ignore this one thread may be gone by itself actually
+                End Try
 
-            _RenderThread = Nothing
-        End If
+                _RenderThread = Nothing
+            End If
+        End SyncLock
         Dim R As New Int32Rect(0, 0, CInt(_Viewer.ActualWidth), CInt(_Viewer.ActualHeight))
         Try
-            ReDim _ImgData(CInt(Math.Ceiling(_Viewer.ActualWidth)) * CInt(Math.Ceiling(_Viewer.ActualHeight)))
+            RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs("Image"))
+            ReDim _ImgData(CInt(Math.Ceiling(_Viewer.ActualWidth)) * CInt(Math.Ceiling(_Viewer.ActualHeight)) - 1)
             '_Img.CopyPixels(R, _ImgData, 4 * CInt(_Viewer.ActualWidth), 0)
             _RenderThread = New Thread(AddressOf ImGRenderThread)
+
             _StopRender = False
             _RenderThread.Start()
         Catch
@@ -206,8 +243,12 @@ Public Class MeteoBitmapper
         Try
             GetImageData()
             ImageReady = True
+            RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs("Image"))
         Finally
-            _RenderThread = Nothing
+            'SyncLock _renderlock
+            '_RenderThread = Nothing
+            'End SyncLock
+
         End Try
     End Sub
 
