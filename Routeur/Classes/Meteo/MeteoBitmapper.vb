c8e769c80bf4286604c74dd3dd6d39854ec639e9
sbs <sbs@34ec630f-655c-4738-8800-74af9c3ddee0>
2012-12-15 19:25:28 +0000

    Intermediate commit before meteoorganizer clean-up

--- Routeur/Classes/Meteo/MeteoBitmapper.vb
+++ Routeur/Classes/Meteo/MeteoBitmapper.vb
@@ -18,8 +18,13 @@ Public Class MeteoBitmapper
     Private _renderlock As New Object
     Private Const NbX As Integer = 20
     Private Const NbY As Integer = 20
-    Dim WindDir(NbX * NbY - 1) As Double
-    Dim WindSpeed(NbX * NbY - 1) As Double
+
+    Private meteocache(12 * 24 * 15, NbX * NbY - 1) As MeteoData
+
+    Private Class MeteoData
+        Property Dir As Double
+        Property Speed As Double = -1
+    End Class
 
 
     Private _Arrow() As Integer = New Integer() {0, 4,
@@ -38,150 +43,128 @@ Public Class MeteoBitmapper
         _Viewer = Viewer
     End Sub
 
-    Public ReadOnly Property Image As WriteableBitmap
-        Get
+    Private Function GetCacheIndex() As Integer
 
-            Try
-                Dim W As Integer = CInt(Math.Ceiling(_Viewer.ActualWidth))
-                Dim H As Integer = CInt(Math.Ceiling(_Viewer.ActualHeight))
-                Dim ImgStride As Integer = CInt(W * ((PixelFormats.Pbgra32.BitsPerPixel) / 8))
-                Dim R As New Int32Rect(0, 0, CInt(W), CInt(H))
-                Dim CPt(3) As Point
-                CPt(0) = New Point(0, -20)
-                CPt(1) = New Point(-5, 5)
-                CPt(2) = New Point(0, 0)
-                CPt(3) = New Point(5, 5)
-                Dim d(3) As Double
+    End Function
 
-                For n As Integer = 0 To 3
-                    d(n) = Sqrt(CPt(n).X * CPt(n).X + CPt(n).Y * CPt(n).Y)
-                Next
-                Debug.Assert(Thread.CurrentThread.ManagedThreadId = _Viewer.Dispatcher.Thread.ManagedThreadId)
+    'Public ReadOnly Property Image As WriteableBitmap
+    '    Get
 
-                If _Img Is Nothing OrElse _Img.Width <> W OrElse _Img.Height <> H Then
-                    _Img = New WriteableBitmap(W, H, 96, 96, PixelFormats.Pbgra32, Nothing)
-                End If
-                Using _Img.GetBitmapContext
-                    '_Img.WritePixels(R, _ImgData, ImgStride, 0)
-                    _Img.Clear()
-                    For x As Integer = 0 To NbX - 1
-                        For y As Integer = 0 To NbY - 1
-                            Dim pt(3) As Point
-                            Dim Scale As Double = Math.Log(WindSpeed(x + NbX * y) + 1)
-                            Dim WindColor As Integer = WindColors.GetColor(WindSpeed(x + NbX * y))
+    '        Try
+    '            Dim W As Integer = CInt(Math.Ceiling(_Viewer.ActualWidth))
+    '            Dim H As Integer = CInt(Math.Ceiling(_Viewer.ActualHeight))
+    '            Dim ImgStride As Integer = CInt(W * ((PixelFormats.Pbgra32.BitsPerPixel) / 8))
+    '            Dim R As New Int32Rect(0, 0, CInt(W), CInt(H))
+    '            Dim CPt(3) As Point
+    '            CPt(0) = New Point(0, -20)
+    '            CPt(1) = New Point(-5, 5)
+    '            CPt(2) = New Point(0, 0)
+    '            CPt(3) = New Point(5, 5)
+    '            Dim d(3) As Double
 
-                            For n As Integer = 0 To 3
-                                'Scale Arrow
-                                pt(n) = New Point(CPt(n).X * Scale, CPt(n).Y * Scale)
+    '            For n As Integer = 0 To 3
+    '                d(n) = Sqrt(CPt(n).X * CPt(n).X + CPt(n).Y * CPt(n).Y)
+    '            Next
+    '            Debug.Assert(Thread.CurrentThread.ManagedThreadId = _Viewer.Dispatcher.Thread.ManagedThreadId)
 
-                                'Rotate
-                                Dim a As Double = Atan2(pt(n).Y, pt(n).X) - WindDir(x + NbX * y)
-                                pt(n).X = d(n) * Cos(a)
-                                pt(n).Y = d(n) * Sin(a)
+    '            If _Img Is Nothing OrElse _Img.Width <> W OrElse _Img.Height <> H Then
+    '                _Img = New WriteableBitmap(W, H, 96, 96, PixelFormats.Pbgra32, Nothing)
+    '            End If
+    '            Using _Img.GetBitmapContext
+    '                '_Img.WritePixels(R, _ImgData, ImgStride, 0)
+    '                _Img.Clear()
+    '                For x As Integer = 0 To NbX - 1
+    '                    For y As Integer = 0 To NbY - 1
 
-                                'Offset
-                                pt(n).X += W / (NbX - 1) * x
-                                pt(n).Y += H / (NbY - 1) * y
-                            Next
+    '                        If WindSpeed(x + NbX * y) <> -1 Then
+    '                            Dim pt(3) As Point
+    '                            Dim Scale As Double = Math.Log(WindSpeed(x + NbX * y) + 1)
+    '                            Dim WindColor As Integer = WindColors.GetColor(WindSpeed(x + NbX * y))
 
-                            _Img.FillQuad(CInt(pt(0).X), CInt(pt(0).Y), CInt(pt(1).X), CInt(pt(1).Y), CInt(pt(2).X), CInt(pt(2).Y), CInt(pt(3).X), CInt(pt(3).Y), WindColor)
+    '                            For n As Integer = 0 To 3
+    '                                'Scale Arrow
+    '                                pt(n) = New Point(CPt(n).X * Scale, CPt(n).Y * Scale)
 
-                            'Dim Txt As New TextBlock
-                            'Txt.Text = WindSpeed(x + NbX * y).ToString("0Kts") & "/" & (WindDir(x + NbX * y) / PI * 180).ToString("0.0°")
-                            'Txt.Foreground = New SolidColorBrush(Color.FromRgb(0, 0, 0))
-                            'Txt.FontSize = 7
-                            'Dim tbmp As New WriteableBitmap(40, 10, 96, 96, PixelFormats.Pbgra32, Nothing)
-                            'tbmp.render()
-                        Next
-                    Next
-                End Using
+    '                                'Rotate
+    '                                Dim a As Double = Atan2(pt(n).Y, pt(n).X) - WindDir(x + NbX * y)
+    '                                pt(n).X = d(n) * Cos(a)
+    '                                pt(n).Y = d(n) * Sin(a)
 
-            Catch ex As Exception
-                Dim i As Integer = 0
-            End Try
+    '                                'Offset
+    '                                pt(n).X += W / (NbX - 1) * x
+    '                                pt(n).Y += H / (NbY - 1) * y
+    '                            Next
 
-            ImageReady = False
+    '                            _Img.FillQuad(CInt(pt(0).X), CInt(pt(0).Y), CInt(pt(1).X), CInt(pt(1).Y), CInt(pt(2).X), CInt(pt(2).Y), CInt(pt(3).X), CInt(pt(3).Y), WindColor)
 
-            Return _Img
-        End Get
-    End Property
+    '                            'Dim Txt As New TextBlock
+    '                            'Txt.Text = WindSpeed(x + NbX * y).ToString("0Kts") & "/" & (WindDir(x + NbX * y) / PI * 180).ToString("0.0°")
+    '                            'Txt.Foreground = New SolidColorBrush(Color.FromRgb(0, 0, 0))
+    '                            'Txt.FontSize = 7
+    '                            'Dim tbmp As New WriteableBitmap(40, 10, 96, 96, PixelFormats.Pbgra32, Nothing)
+    '                            'tbmp.render()
+    '                        End If
+
+    '                    Next
+    '                Next
+    '            End Using
+
+    '        Catch ex As Exception
+    '            Dim i As Integer = 0
+    '        End Try
+
+    '        ImageReady = False
+
+    '        Return _Img
+    '    End Get
+    'End Property
 
     Public Property ImageReady As Boolean = False
 
     Private Sub GetImageData()
-        Dim R As New Int32Rect(0, 0, CInt(_Viewer.ActualWidth), CInt(_Viewer.ActualHeight))
-        Dim Dx As Integer = CInt(_Viewer.ActualWidth / 10)
-        Dim Dy As Integer = CInt(_Viewer.ActualHeight / 10)
-        Dim Start As DateTime = Now
-        Dim MeteoCount As Integer = 0
-        Dim MeteoMs As Double = 0
-        Dim Ix As Integer = 0
-        Dim Iy As Integer = 0
-        For Ix = 0 To NbX - 1
-            Dim X As Integer = CInt(Ix * _Viewer.ActualWidth / NbX)
 
-            For Iy = 0 To NbY - 1
-                Dim Y As Integer = CInt(Iy * _Viewer.ActualHeight / NbY)
-                Dim MiFail As Boolean = False
-
-                If _StopRender Then Return
-                Dim StartMeteo As DateTime = Now
-                Dim mi As MeteoInfo = _meteo.GetMeteoToDate(New Coords(_Viewer.CanvasToLat(Y), _Viewer.CanvasToLon(X)), Me.Date, True)
-                If mi IsNot Nothing Then
-                    WindSpeed(CInt(Ix + NbX * Iy)) = mi.Strength
-                    Dim Index As Integer = NbX * Iy + Ix
-                    If Index < WindDir.Count Then
-                        WindDir(Index) = (180 - mi.Dir) / 180 * PI
-                    End If
-                Else
-                    MiFail = True
-                    Exit For
-                End If
-                If _StopRender Then
-                    Return
-                End If
-                MeteoCount += 1
-                MeteoMs += Now.Subtract(StartMeteo).TotalMilliseconds
-
-            Next
-
-        Next
-        Console.WriteLine("Meteo avg" & MeteoMs / MeteoCount)
-
-        'For x = 0 To Dx - 1
-        '    Dim S1 As Double = i * (V(1) - V(0)) / (Dx - 1) + V(0)
-        '    Dim S2 As Double = i * (V(3) - V(2)) / (Dx - 1) + V(2)
-
-        '    For j = 0 To Dy - 1
-
-        '        If CInt((y + j) * _Viewer.ActualWidth + x + i) < _ImgData.Length Then
-        '            Dim S As Double = (S2 - S1) / (Dy - 1) * j + S1
-        '            If S > 70 Then
-        '                S = 70
-        '            End If
-        '            'Dim SIndex As Integer = CInt(Math.Round(S * 10))
-        '            'Points(BITMAP_SIZE * j + i) = V
-        '            'SyncLock WindColors.WindColorGDIBrushes(SIndex)
-        '            ' G.FillRectangle(WindColors.WindColorGDIBrushes(SIndex), New System.Drawing.RectangleF(CSng(i), CSng(j), 1.0, 1.0))
-        '            'End SyncLock
-        '            Dim C As Integer = WindColors.GetColor(S)
-        '            Try
-        '                _ImgData(CInt((y + j) * _Viewer.ActualWidth + x + i)) = C
-        '            Catch
-        '                'something when wrong, return
-        '                Return
-        '            End Try
-
-        '        End If
-        '        If _StopRender Then
-        '            Return
-        '        End If
-        '    Next
-        'Next
-
-
-        Console.WriteLine("Meteo duration" & Now.Subtract(Start).TotalSeconds)
     End Sub
+    'Private Sub GetImageData()
+    '    Dim R As New Int32Rect(0, 0, CInt(_Viewer.ActualWidth), CInt(_Viewer.ActualHeight))
+    '    Dim Dx As Integer = CInt(_Viewer.ActualWidth / 10)
+    '    Dim Dy As Integer = CInt(_Viewer.ActualHeight / 10)
+    '    Dim Start As DateTime = Now
+    '    Dim MeteoCount As Integer = 0
+    '    Dim MeteoMs As Double = 0
+    '    Dim Ix As Integer = 0
+    '    Dim Iy As Integer = 0
+    '    For Ix = 0 To NbX - 1
+    '        Dim X As Integer = CInt(Ix * _Viewer.ActualWidth / NbX)
+
+    '        For Iy = 0 To NbY - 1
+    '            Dim Y As Integer = CInt(Iy * _Viewer.ActualHeight / NbY)
+    '            Dim MiFail As Boolean = False
+
+    '            If _StopRender Then Return
+    '            Dim StartMeteo As DateTime = Now
+    '            Dim mi As MeteoInfo = _meteo.GetMeteoToDate(New Coords(_Viewer.CanvasToLat(Y), _Viewer.CanvasToLon(X)), Me.Date, True)
+    '            If mi IsNot Nothing Then
+    '                WindSpeed(CInt(Ix + NbX * Iy)) = mi.Strength
+    '                WindDir(CInt(Ix + NbX * Iy)) = (180 - mi.Dir) / 180 * PI
+
+    '            Else
+    '                WindSpeed(CInt(Ix + NbX * Iy)) = -1
+
+    '            End If
+    '            If _StopRender Then
+    '                Return
+    '            End If
+    '            MeteoCount += 1
+    '            MeteoMs += Now.Subtract(StartMeteo).TotalMilliseconds
+
+    '        Next
+
+    '    Next
+    '    Console.WriteLine("Meteo avg" & MeteoMs / MeteoCount)
+
+
+    '    Console.WriteLine("Meteo duration" & Now.Subtract(Start).TotalSeconds)
+    'End Sub
 
     Public Property [Date] As DateTime
         Get
