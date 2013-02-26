5c7e5e7ffa37b20566bcf855345bb6b617de0799
sbsrouteur <sbsrouteur@free.fr>
2013-02-26 23:34:28 +0100

    Factor brush constants

---
+++ Routeur/Classes/Configuration/ConverterStockBrushes.vb
@@ -0,0 +1,6 @@
+﻿Module ConverterStockBrushes
+
+    Public BrushOK As New SolidColorBrush(New Color() With {.A = &HFF, .R = &H7A, .G = &HFF, .B = &HAE})
+    Public BrushNOK As New SolidColorBrush(New Color() With {.A = &HFF, .R = &HFF, .G = &H7A, .B = &H7A})
+
+End Module
