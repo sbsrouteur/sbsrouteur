2d582fa44f158e72a2c4edaef37b316585f1356f
sbsrouteur <sbsrouteur@free.fr>
2013-11-03 23:16:10 +0100

    Add Lat normalization (and fix rolling above limit)

--- Routeur/Classes/Coords.vb
+++ Routeur/Classes/Coords.vb
@@ -52,9 +52,22 @@ Public Class Coords
         End Get
         Set(ByVal value As Double)
             _Lat = value
-            _n_lat = _Lat Mod (PI / 2)
             _Lat_Deg = value / PI * 180
-            _n_lat_deg = _Lat_Deg Mod (90)
+
+            If Math.Abs(_Lat_Deg) > 90 Then
+
+                If _Lat_Deg >= 0 Then
+                    _n_lat_deg = 90 - (_Lat_Deg - 90)
+                    _n_lat = (PI / 2) - (_Lat - (PI / 2))
+                Else
+                    _n_lat_deg = -90 - (_Lat_Deg + 90)
+                    _n_lat = -(PI / 2) - (_Lat + (PI / 2))
+                End If
+
+            Else
+                _n_lat_deg = _Lat_Deg
+            End If
+
         End Set
     End Property
 
@@ -137,6 +150,13 @@ Public Class Coords
     Public ReadOnly Property N_Lat As Double Implements ICoords.N_Lat
         Get
             Return _n_lat
+        End Get
+    End Property
+
+    <XmlIgnore()> _
+    Public ReadOnly Property N_Lat_Deg As Double
+        Get
+            Return _n_lat_deg
         End Get
     End Property
 
@@ -295,7 +315,7 @@ Public Class Coords
 
     Public Function GetHashCode1() As Long Implements ICoords.GetHashCode
         Return CoordsComparer.GetHashCode1(Me)
-        
+
     End Function
 End Class
 
