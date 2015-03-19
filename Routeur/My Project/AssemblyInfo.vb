7e9106c08dc59182a1fdb1038fe6b7a05e7ad260
sbsrouteur <sbsrouteur@free.fr>
2015-03-19 21:46:03 +0100

    Bump version number to 0.38

--- Routeur/My Project/AssemblyInfo.vb
+++ Routeur/My Project/AssemblyInfo.vb
@@ -56,5 +56,5 @@ Imports System.Windows
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.37.0.0")> 
-<Assembly: AssemblyFileVersion("0.37.0.0")> 
+<Assembly: AssemblyVersion("0.38.0.0")> 
+<Assembly: AssemblyFileVersion("0.38.0.0")> 
